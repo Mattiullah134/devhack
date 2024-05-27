3bfb9477a38996c6e379d186c8b5099e4a8d0e9e
Mattiullah <mattikhokhar01@gmail.com>
2024-05-27 00:02:53 -0700

    handle the footer warning

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "@/public/logo.png";
+// import logo from "@/public/logo.png";
 
 const Footer = () => {
   return (
@@ -7,12 +7,6 @@ const Footer = () => {
       <div className="text-gray-600 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <div className="flex lg:flex-1">
-              <div href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img className="h-8 w-auto" src={{ url: logo }} alt="" />
-              </div>
-            </div>
             <span className="ml-3 text-xl">Image Gennie</span>
           </div>
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
